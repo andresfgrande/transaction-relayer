079d4d8800d87da321712e98c4be17b4b93ab605
Andrés Grande <andres.grande@somosinapsis.com>
2023-11-04 01:36:32 +0100

    wip

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-//app.use(cors());
+//app.use(cors());test
 /*app.use(cors({
     origin: 'https://loyalty-market.vercel.app'
   }));*/
